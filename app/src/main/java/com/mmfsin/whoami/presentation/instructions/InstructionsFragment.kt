716d535f31faf174800ee8a47fe4ae536e0bdb21
Geraquem <geraquem@gmail.com>
2023-09-05 12:26:34 +0200

    wip

--- app/src/main/java/com/mmfsin/whoami/presentation/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/mmfsin/whoami/presentation/instructions/InstructionsFragment.kt
@@ -1,9 +1,15 @@
 package com.mmfsin.whoami.presentation.instructions
 
 
+import android.animation.LayoutTransition.CHANGING
 import android.content.Context
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import com.mmfsin.whoami.base.BaseFragmentNoVM
 import com.mmfsin.whoami.databinding.FragmentInstructionsBinding
 import com.mmfsin.whoami.presentation.MainActivity
@@ -23,7 +29,16 @@ class InstructionsFragment : BaseFragmentNoVM<FragmentInstructionsBinding>() {
     override fun setListeners() {
         binding.apply {
             tvClose.setOnClickListener { (activity as MainActivity).onBackPressed() }
+
+            cvHowToPlay.setOnClickListener { setExpandableView(htpDetails, llHtp) }
         }
+    }
+
+    private fun setExpandableView(expandable: View, linear: LinearLayout) {
+        val v = if (expandable.isVisible) View.GONE else View.VISIBLE
+        TransitionManager.beginDelayedTransition(linear, AutoTransition())
+        expandable.visibility = v
+        linear.layoutTransition.enableTransitionType(CHANGING)
     }
 
     override fun onAttach(context: Context) {
